f1df268b997fdf3c72832b3e2e17a440f1f55570
nowhere <nowhere@nowhere.com>
2020-06-20 23:35:20 +0000

    add -y option in installation to use in CI

--- prerequisite_des.sh
+++ prerequisite_des.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update  && \ 
-sudo apt-get install software-properties-common && \
+sudo apt-get install -y software-properties-common && \
 sudo add-apt-repository -y ppa:jonathonf/vim && \
 sudo apt update && \
 sudo apt install -y vim && \
@@ -9,8 +9,8 @@ sudo apt-get install curl build-essential cmake python-dev libncurses5-dev unzip
        && sudo apt install gnupg ca-certificates -y \
        && sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF \
        && echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list \
-       && sudo apt update && sudo apt install mono-devel -y \
+       && sudo apt update && sudo apt install mono-devel -y && \
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash - && \
-sudo apt install nodejs && \
+sudo apt install -y nodejs && \
 echo finfin
 
