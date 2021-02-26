5ca46d5f35e56936995a187ce57a867cabadda73
windheim <windheim@noe.com>
2021-02-26 05:37:25 +0000

    remove useless apt list

--- prerequisite_des.sh
+++ prerequisite_des.sh
@@ -15,7 +15,6 @@ sudo apt-get install curl build-essential cmake python-dev libncurses5-dev unzip
 	&& curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
        && sudo apt install gnupg ca-certificates -y \
        && sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF \
-       && echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list \
        && sudo apt update && sudo apt install mono-devel -y && \
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash - && \
 sudo apt install -y nodejs 
