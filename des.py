e54b65d386759ea29f39c0daca10c5e4ae88799d
nowhere <nowhere@nowhere.com>
2025-08-27 08:36:41 +0900

    disable lsp diagnostics

--- des.py
+++ des.py
@@ -267,6 +267,7 @@ augroup lsp_install
     " call s:on_lsp_buffer_enabled only for languages that has the server registered.
     autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled()
 augroup END
+let g:lsp_diagnostics_enabled = 0
 
 " vim-lsp 설정 end ===============================
 
