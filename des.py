6b7856c410245ef1c07f1168d8c83876871c2843
nowhere <nowhere@nowhere.com>
2025-12-27 03:45:47 +0000

    disable diagnostic msg

--- des.py
+++ des.py
@@ -269,7 +269,7 @@ augroup lsp_install
     " call s:on_lsp_buffer_enabled only for languages that has the server registered.
     autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled()
 augroup END
-
+let g:lsp_diagnostics_enabled = 0
 " vim-lsp 설정 end ===============================
 
 "highlight 색깔 변경
